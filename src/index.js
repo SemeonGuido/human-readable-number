227e75c72ca6f068ba999a6a724b91eb7cc9ee47
Alexandr <k.v.alexandr@gmail.com>
2020-10-07 19:42:04 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+  const readOne = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const readTen = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+  if (number === 0) return 'zero';
+
+  const hundreds = Math.floor(number / 100);
+  const tens = number - hundreds * 100;
+  const tenL = Math.floor(tens / 10);
+  const tenR = tens % 10;
+
+  const result = [];
+  if (hundreds > 0) {
+    result.push(`${readOne[hundreds - 1]} hundred`);
+  }
+
+  if (tenL > 1) {
+    result.push(`${readTen[tenL - 2]}`);
+
+    if (tenR > 0) {
+      result.push(`${readOne[tenR - 1]}`);
+    }
+  } else {
+    if (readOne[tens - 1]) result.push(`${readOne[tens - 1]}`);
+  }
+
+  return result.join(' ');
 }
